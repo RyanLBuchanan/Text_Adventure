3585b16c8dfca43add237aac1d5c801d870322ea
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-11-20 17:20:16 -0700

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text displayText1;
+    public Text displayText2;
+    public InputAction[] inputActions;
+
+    [HideInInspector] public RoomNavigation roomNavigation;
+    [HideInInspector] public List<string> interactionDescriptionInRooms = new List<string>();
+    [HideInInspector] public InteractableItems interactableItems;
+
+    List<string> actionLog = new List<string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactableItems = GetComponent<InteractableItems>();
+        roomNavigation = GetComponent<RoomNavigation> ();
+    }
+
+    private void Start()
+    {
+        DisplayRoomText ();
+        DisplayLoggedText ();
+    }
+
+    public void DisplayLoggedText()
+    {
+
+        string logAsText = string.Join("\n", actionLog.ToArray ());
+
+        displayText1.text = logAsText;
+        displayText2.text = logAsText;
+    }
+
+    public void DisplayRoomText()
+    {
+        ClearCollecitonsForNewRoom();
+
+        UnpackRoom();
+
+        string joinedInteractionDescriptions = string.Join("\n", interactionDescriptionInRooms.ToArray());
+
+        string combinedText = roomNavigation.currentRoom.description + "\n" + joinedInteractionDescriptions;
+
+        LogStringWithReturn(combinedText);
+    }
+
+    void UnpackRoom()
+    {
+        roomNavigation.UnpackExitsInRoom();
+        PrepareObjectsToTakeOrExamine(roomNavigation.currentRoom);
+    }
+
+    void PrepareObjectsToTakeOrExamine(Room currentRoom)
+    {
+        for (int i = 0; i < currentRoom.interactableObjectsInRoom.Length; i++)
+        {
+            string descriptionNotInInventory = interactableItems.GetObjectsNotInInventory(currentRoom, i);
+            if (descriptionNotInInventory != null)
+            {
+                interactionDescriptionInRooms.Add(descriptionNotInInventory);
+            }
+
+            InteractableObject interactableInRoom = currentRoom.interactableObjectsInRoom[i];
+
+            for (int j = 0; j < interactableInRoom.interactions.Length; j++)
+            {
+                Interaction interaction = interactableInRoom.interactions[j];
+                if (interaction.inputAction.keyWord == "examine")
+                {
+                    interactableItems.examineDictionary.Add(interactableInRoom.noun, interaction.textResponse);
+                }
+
+                if (interaction.inputAction.keyWord == "take")
+                {
+                    interactableItems.takeDictionary.Add(interactableInRoom.noun, interaction.textResponse);
+                }
+            }
+        }
+    }
+
+    public string TestVerbDictionaryWithNoun(Dictionary<string, string> verbDictionary, string verb, string noun)
+    {
+        if (verbDictionary.ContainsKey(noun))
+        {
+            return verbDictionary[noun];
+        }
+
+        return "You can't " + verb + " " + noun;
+    
+    }
+
+    void ClearCollecitonsForNewRoom()
+    {
+        interactableItems.ClearCollections();
+        interactionDescriptionInRooms.Clear();
+        roomNavigation.ClearExits();
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
